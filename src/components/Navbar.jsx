1f5ab0c0295b1e0559d2c103e8df8e1552ba0d41
prince saspara <princesaspara46@gmail.com>
2023-08-07 15:30:50 +0530

    footer component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const Navbar = () => {
+function Navbar({ title }) {
   return (
-    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-secondary-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl" />
           <Link to="/" className="text-lg font-bold align-middle">
-            Github Finder
+            {title}
           </Link>
         </div>
+
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
             <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
@@ -24,6 +26,14 @@ const Navbar = () => {
       </div>
     </nav>
   );
+}
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Navbar;
