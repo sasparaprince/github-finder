4f7256a938e89ea265b575feef6b216fb4a80439
prince saspara <princesaspara46@gmail.com>
2023-08-08 14:38:18 +0530

    Alert Context & Reducer

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,34 @@
+import { createContext, useReducer } from "react";
+import AlertReducer from "./AlertReducer";
+
+
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+  //set Slert
+
+  const setAlert = (msg,type) => {
+    dispatch({
+        type: 'SET_ALERT',
+        payload: {msg,type},
+    })
+    setTimeout(()=>({type: 'REMOVI_ALERT'}), 3000);
+  }
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,setAlert
+      }}
+    >
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertContext;
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,7 +3,7 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-// const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+// const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 // const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
