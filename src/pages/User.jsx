2fa799922992d84ee38fc2f27ef8dd1ad801613c
prince saspara <princesaspara46@gmail.com>
2023-08-08 16:54:44 +0530

    User Profile Stats

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -115,44 +115,44 @@ const User = () => {
             </div>
           </div>
         </div>
-        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
-          <div className="grid grid-cols-1 md:grid-cols-4">
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <FaUsers className="text-3xl md:text-5xl" />
+        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+          <div className='grid grid-cols-1 md:grid-cols-4'>
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaUsers className='text-3xl md:text-5xl' />
               </div>
-              <div className="stat-title pr-5">Followers</div>
-              <div className="stat-value pr-5 text-3xl md:text-4xl">
+              <div className='stat-title pr-5'>Followers</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {followers}
               </div>
             </div>
 
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <FaUserFriends className="text-3xl md:text-5xl" />
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaUserFriends className='text-3xl md:text-5xl' />
               </div>
-              <div className="stat-title pr-5">Following</div>
-              <div className="stat-value pr-5 text-3xl md:text-4xl">
+              <div className='stat-title pr-5'>Following</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {following}
               </div>
             </div>
 
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <FaCodepen className="text-3xl md:text-5xl" />
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaCodepen className='text-3xl md:text-5xl' />
               </div>
-              <div className="stat-title pr-5">Public Repos</div>
-              <div className="stat-value pr-5 text-3xl md:text-4xl">
+              <div className='stat-title pr-5'>Public Repos</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {public_repos}
               </div>
             </div>
 
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <FaStore className="text-3xl md:text-5xl" />
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaStore className='text-3xl md:text-5xl' />
               </div>
-              <div className="stat-title pr-5">Public Gists</div>
-              <div className="stat-value pr-5 text-3xl md:text-4xl">
+              <div className='stat-title pr-5'>Public Gists</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
                 {public_gists}
               </div>
             </div>
